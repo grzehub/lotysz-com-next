c8d67f7a526783c889a7e2c289dace581aa41503
Grzegorz Łotysz <mustard@MacBook-Air-Grzegorz.local>
2022-03-07 00:54:15 +0100

    clean up

--- lib/posts.ts
+++ lib/posts.ts
@@ -3,7 +3,6 @@ import path from "path";
 import matter from "gray-matter";
 import { remark } from "remark";
 import html from "remark-html";
-import { ProjectProps } from "../pages";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -83,23 +82,23 @@ export async function getPostData(id: string | string[] | undefined) {
   };
 }
 
-export async function getStrapiPostData({
-  params,
-}: {
-  params: { id: string };
-}) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`http://localhost:1337/projects/${params.id}`);
-  const project: ProjectProps[] = await res.json();
+// export async function getStrapiPostData({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   // params contains the post `id`.
+//   // If the route is like /posts/1, then params.id is 1
+//   const res = await fetch(`http://localhost:1337/projects/${params.id}`);
+//   const project: ProjectProps[] = await res.json();
 
-  if (!project) {
-    return {
-      notFound: true,
-    };
-  }
+//   if (!project) {
+//     return {
+//       notFound: true,
+//     };
+//   }
 
-  return {
-    props: { project }, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: { project }, // will be passed to the page component as props
+//   };
+// }
